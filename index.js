f4a05c717e668d61d8d04b4bb0610572ec9d205b
Glen <jiayuan.xu@mail.mcgill.ca>
2021-11-09 19:13:28 -0500

    1-3

--- index.js
+++ index.js
@@ -1,26 +1,9 @@
 const express = require('express');
+const { string } = require('joi');
 const app = express();
 //const utils = require('./utils/task-schema.js')
 
 app.use(express.json());
-
-// const tasks = [
-//     {
-//         id: 1,
-//         name: "Task 1",
-//         completed: false
-//     },
-//     {
-//         id: 2,
-//         name: "Task 2",
-//         completed: false
-//     },
-//     {
-//         id: 3,
-//         name: "Task 3",
-//         completed: false
-//     }
-// ]
 
 app.post("/calc", (request, response) => {
 
@@ -29,129 +12,136 @@ app.post("/calc", (request, response) => {
     // weight, unit_of_weight
     // whether_standard
 
-    if (!(request.body.length_unit == "mm" || request.body.length_unit == "inch")){
-        response.status(400).send("Envelope unit for length is neither the accepted mm or inch.");
+    // 0. test if all params exist
+
+    // 0.1 width/length/weight
+
+    if (!(request.body.hasOwnProperty('width'))) {
+        response.status(400).send("the input WIDTH is missing. ");
         return;
     }
 
-    if (request.body.length < 0) {
-        response.status(400).send("Envelope length received is not positive.");
+    if (!(request.body.hasOwnProperty('length'))) {
+        response.status(400).send("the input LENGTH is missing. ");
         return;
     }
-    if (request.body.length > 380) {
-        response.status(400).send("Envelope length received exceeds maximal 380 mm.");
+
+    if (!(request.body.hasOwnProperty('weight'))) {
+        response.status(400).send("the input WEIGHT is missing. ");
+        return;
+    }
+
+    // 0.2 unit_of_width/unit_of_length/unit_of_weight
+
+    if (!(request.body.hasOwnProperty('unit_of_width'))) {
+        response.status(400).send("the input UNIT_OF_WIDTH is missing. ");
+        return;
+    }
+
+    if (!(request.body.hasOwnProperty('unit_of_length'))) {
+        response.status(400).send("the input UNIT_OF_LENGTH is missing. ");
+        return;
+    }
+
+    if (!(request.body.hasOwnProperty('unit_of_weight'))) {
+        response.status(400).send("the input UNIT_OF_WEIGHT is missing. ");
+        return;
+    }
+
+    // 0.3 whether_standard
+
+    if (!(request.body.hasOwnProperty('whether_standard'))) {
+        response.status(400).send("the input WHETHER_STANDARD is missing. ");
+        return;
+    }
+
+    // 1. test if types of params are correct 
+    
+    // 1.1 width/length/weight type should be numbers
+
+    if (!parseFloat(request.body.width)) {
+        response.status(400).send("the input WIDTH is not a number. ");
+        return;
+    }
+
+    if (!parseFloat(request.body.length)) {
+        response.status(400).send("the input LENGTH is not a number. ");
+        return;
+    }
+
+    if (!parseFloat(request.body.weight)) {
+        response.status(400).send("the input WEIGHT is not a number. ");
+        return;
+    }
+
+    const width = parseFloat(request.body.width);
+    const length = parseFloat(request.body.length);
+    const weight = parseFloat(request.body.weight);
+
+    // 1.2 unit_of_width/unit_of_length/unit_of_weight should be strings
+
+    if (typeof(request.body.unit_of_width) !== "string") {
+        response.status(400).send("the input UNIT_OF_WIDTH is not a string. ");
+        return;
+    }
+
+    if (typeof(request.body.unit_of_length) !== "string") {
+        response.status(400).send("the input UNIT_OF_LENGTH is not a string. ");
+        return;
+    }
+
+    if (typeof(request.body.unit_of_weight) !== "string") {
+        response.status(400).send("the input UNIT_OF_WEIGHT is not a string. ");
+        return;
+    }
+    
+    // 1.3 whether_standard should be boolean
+
+    if (!(request.body.whether_standard.toLowerCase() == "true" || request.body.whether_standard.toLowerCase() == "false" )) {
+        response.status(400).send("the input WHETHER_STANDARD is not a boolean. ");
+        return;
+    }
+
+    // 2 test if values of params are correct
+
+    // 2.1 width/length/weight should be positive
+
+    if (width <= 0) {
+        response.status(400).send("the input WIDTH should be a positive value. ");
+        return;
+    }
+
+    if (length <= 0) {
+        response.status(400).send("the input LENGTH should be a positive value. ");
+        return;
+    }
+
+    if (weight <= 0) {
+        response.status(400).send("the input WEIGHT should be a positive value. ");
+        return;
+    }
+
+
+    // 2.2 unit_of_width/unit_of_length should be "mm"/"inch",
+    //     unit_of_weight should be "gram"/"ounce"
+
+    if (!(request.body.unit_of_width == "mm" || request.body.unit_of_width == "inch")) {
+        response.status(400).send("the input UNIT_OF_WIDTH should be \"mm\" or \"inch\". ");
+        return;
+    }
+
+    if (!(request.body.unit_of_length == "mm" || request.body.unit_of_length == "inch")) {
+        response.status(400).send("the input UNIT_OF_LENGTH should be \"mm\" or \"inch\". ");
+        return;
+    }
+
+    if (!(request.body.unit_of_weight == "gram" || request.body.unit_of_weight == "ounce")) {
+        response.status(400).send("the input UNIT_OF_WEIGHT should be \"gram\" or \"ounce\". ");
         return;
     }
 
 
 });
 
-
-// // GET
-// app.get("/api/tasks", (request, response) => {
-//     response.send(tasks);
-// })
-
-// //GET by id
-// app.get("/api/tasks/:id", (request, response) => {
-//     const taskId = request.params.id;
-//     const task = tasks.find(task => task.id === parseInt(taskId));
-//     if (!task) {
-//         return response.status(404).send("The task with the provided ID does not exist.")
-//     }
-//     response.send(task);
-// });
-
-// const {taskSchema} = require('./utils/task-schema.js')
-
-// // POST
-
-// app.post("/api/tasks", (request, response) => {
-
-//     const {error, value} = taskSchema.validate(request.body);
-
-//     if (error) return response.status(400).send('The name should be at least 3 chars long!');
-
-//     //lets create the new object we will pass to the tasks array
-
-//     const task = {
-//        id: tasks.length + 1,
-//        name: request.body.name,
-//        completed: request.body.completed
-//     }
-
-//     //lets push in to the tasks array
-
-//     tasks.push(task);
-
-//     response.send(task);
-
-// });
-
-
-// // PUT
-// app.put("/api/tasks/:id", (request, response) => {
-//     const taskId = request.params.id;
-//     const task = tasks.find(task => task.id === parseInt(taskId));
-//     if (!task) {
-//         return response.status(404).send("The task with the provided ID does not exist.")
-//     }
-
-//     const {error, value} = taskSchema.validate(request.body);
-
-//     if (error) return response.status(400).send('The name should be at least 3 chars long!');
-
-//     //lets create the new object we will pass to the tasks array
-
-
-//     task.name = request.body.name,
-//     task.completed = request.body.completed
-
-
-//     response.send(task);
-
-// });
-
-
-// // PATCH
-// app.patch("/api/tasks/:id", (request, response) => {
-//     const taskId = request.params.id;
-//     const task = tasks.find(task => task.id === parseInt(taskId));
-//     if (!task) {
-//         return response.status(404).send("The task with the provided ID does not exist.")
-//     }
-
-//     const {error, value} = taskSchema.validate(request.body);
-
-//     if (error) return response.status(400).send('The name should be at least 3 chars long!');
-
-//     //lets create the new object we will pass to the tasks array
-
-
-//     task.name = request.body.name;
-
-//     if (request.body.completed) {
-//         task.completed = request.body.completed
-//     }
-
-//     response.send(task);
-
-// });
-
-
-// // DELETE
-// app.delete("/api/tasks/:id", (request, response) => {
-//     const taskId = request.params.id;
-//     const task = tasks.find(task => task.id === parseInt(taskId));
-//     if (!task) {
-//         return response.status(404).send("The task with the provided ID does not exist.")
-//     }
-
-//     const index = tasks.indexOf(task);
-//     tasks.splice(index, 1);
-//     response.send(task);
-// });
-
-
 const port = process.env.PORT || 3000;
-app.listen(port, () => console.log('listening on port ${port}...'));
+app.listen(port, () => console.log(`listening on port ${port}...`));
